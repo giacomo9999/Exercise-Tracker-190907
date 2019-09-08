ffee9cac7980d1ca1327f2f713297b3d326033f3
James Gary <gmarchesi888@gmail.com>
2019-09-08 10:54:20 -0400

    All backend routes working correctly.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 const uri = process.env.ATLAS_URI;
 
@@ -17,5 +17,12 @@ const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB Connected With Great Success.");
 });
+
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.get("/", (req, res) => res.send("Hello World"));
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
